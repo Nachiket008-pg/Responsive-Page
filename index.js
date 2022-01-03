951a27c7307bb78d529215383196d2f21ab6dcdb
Nachiket Pedgaonkar <93022151+Nachiket008-pg@users.noreply.github.com>
2022-01-03 12:15:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var menulist = document.getElementById("list");
+list.style.maxHeight = "0px";
+
+function toggemenu(){
+if ( list.style.maxHeight = "0px"){
+    list.style.maxHeight = "120px" ;
+}
+else{
+    list.style.maxHeight = "0px" ;
+}
+}
